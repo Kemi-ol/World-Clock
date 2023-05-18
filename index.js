0cd8c1f70511cdb3321eae702057091c84a7f672
Kemi-ol <130588372+Kemi-ol@users.noreply.github.com>
2023-05-19 00:32:00 +0100

    Added current location time
    added local time using moment.tz.guess()

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function changeDate(event) {
-  let zone = event.target.value;
-  let cityName = zone.replace("_", " ").split("/")[1];
-  let dateZone = moment.tz(zone).format("MMMM Do, YYYY");
+  let timeZone = event.target.value;
+  if(timeZone=== "current"){
+    timeZone= moment.tz.guess();
+  }
+  let cityName = timeZone.replace("_", " ").split("/")[1];
+  let dateZone = moment.tz(timeZone).format("MMMM Do, YYYY");
   let city = document.querySelector("#cities");
-  let time = moment.tz(zone).format("HH:mm:ss");
+  let time = moment.tz(timeZone).format("HH:mm:ss");
   city.innerHTML = `<div class="city" id="los-angeles">
         <div>
 <h2>${cityName}</h2>
 <div class="date" id="date">${dateZone}</div>
-</div>
+</div>                        
 <div class="time" id="time">${time}<small>PM</small></div>
     </div>`;
 }
@@ -32,3 +35,4 @@ setInterval(function () {
     .tz("Europe/London")
     .format("HH:mm:ss [<small>]A[</small>]");
 }}, 1000);
+
