7b380f53fd8290729ab2b250f92d2cfd68e558a0
Kemi-ol <130588372+Kemi-ol@users.noreply.github.com>
2023-05-19 00:16:40 +0100

    Select Time zone added
    Added Time zones for each select city and home page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function changeDate(event) {
   let zone = event.target.value;
+  let cityName = zone.replace("_", " ").split("/")[1];
   let dateZone = moment.tz(zone).format("MMMM Do, YYYY");
-  let city = document.querySelector("#los-angeles");
+  let city = document.querySelector("#cities");
   let time = moment.tz(zone).format("HH:mm:ss");
   city.innerHTML = `<div class="city" id="los-angeles">
         <div>
-<h2>${zone}</h2>
+<h2>${cityName}</h2>
 <div class="date" id="date">${dateZone}</div>
 </div>
 <div class="time" id="time">${time}<small>PM</small></div>
@@ -14,20 +15,20 @@ function changeDate(event) {
 let cityElement = document.querySelector("#city");
 cityElement.addEventListener("change", changeDate);
 
-setInterval(function(){
-//los-angeles
-let laDate = document.querySelector("#losDate");
-let laTime = document.querySelector("#losTime");
-laDate.innerHTML = moment.tz("America / Los_Angeles").format("MMMM Do, YYYY");
-laTime.innerHTML = moment
-  .tz("America / Los_Angeles")
-  .format("HH:mm:ss [<small>]A[</small>]");
-
-  //tokyo
-let Tokyodate = document.querySelector("#tokyo-date");
-let Tokyotime = document.querySelector("#tokyo-time");
-Tokyodate.innerHTML = moment.tz("Asia/Tokyo").format("MMMM Do, YYYY");
-Tokyotime.innerHTML = moment
-  .tz("Asia/Tokyo")
-  .format("HH:mm:ss [<small>]A[</small>]");
-},1000);
+setInterval(function () {
+  //los-angeles
+  let laDate = document.querySelector("#losDate");
+  let laTime = document.querySelector("#losTime");
+  if (laDate){
+  laDate.innerHTML = moment.tz("America/Los_Angeles").format("MMMM Do, YYYY");
+  laTime.innerHTML = moment.tz("America/Los_Angeles").format("HH:mm:ss[<small>]A[</small>]");
+  }
+  //london
+  let londonDate = document.querySelector("#london-date");
+  let londonTime = document.querySelector("#london-time");
+  if(londonDate){
+  londonDate.innerHTML = moment.tz("Europe/London").format("MMMM Do, YYYY");
+  londonTime.innerHTML = moment
+    .tz("Europe/London")
+    .format("HH:mm:ss [<small>]A[</small>]");
+}}, 1000);
