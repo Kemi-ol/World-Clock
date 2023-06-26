9aad6b2f9e844631fd5e16f5de2f937004c1e6dc
Kemi-ol <130588372+Kemi-ol@users.noreply.github.com>
2023-06-27 00:26:37 +0100

    updated the classes
    updated classes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function changeDate(event) {
   let timeZone = event.target.value;
-  if(timeZone=== "current"){
-    timeZone= moment.tz.guess();
+  if (timeZone === "current") {
+    timeZone = moment.tz.guess();
   }
   let cityName = timeZone.replace("_", " ").split("/")[1];
   let dateZone = moment.tz(timeZone).format("MMMM Do, YYYY");
@@ -22,28 +22,30 @@ setInterval(function () {
   //los-angeles
   let laDate = document.querySelector("#losDate");
   let laTime = document.querySelector("#losTime");
-  if (laDate){
-  laDate.innerHTML = moment.tz("America/Los_Angeles").format("MMMM Do, YYYY");
-  laTime.innerHTML = moment.tz("America/Los_Angeles").format("HH:mm:ss[<small>]A[</small>]");
+  if (laDate) {
+    laDate.innerHTML = moment.tz("America/Los_Angeles").format("MMMM Do, YYYY");
+    laTime.innerHTML = moment
+      .tz("America/Los_Angeles")
+      .format("HH:mm:ss[<small>]A[</small>]");
   }
   //london
   let londonDate = document.querySelector("#london-date");
   let londonTime = document.querySelector("#london-time");
-  if(londonDate){
-  londonDate.innerHTML = moment.tz("Europe/London").format("MMMM Do, YYYY");
-  londonTime.innerHTML = moment
-    .tz("Europe/London")
-    .format("HH:mm:ss [<small>]A[</small>]");
-}
-//france
-let africaDate = document.querySelector("#africa-date");
+  if (londonDate) {
+    londonDate.innerHTML = moment.tz("Europe/London").format("MMMM Do, YYYY");
+    londonTime.innerHTML = moment
+      .tz("Europe/London")
+      .format("HH:mm:ss [<small>]A[</small>]");
+  }
+  //france
+  let africaDate = document.querySelector("#africa-date");
   let africaTime = document.querySelector("#africa-time");
-  if(africaDate){
-  africaDate.innerHTML = moment
-    .tz("Africa/Johannesburg")
-    .format("MMMM Do, YYYY");
-  africaTime.innerHTML = moment
-    .tz("Africa/Johannesburg")
-    .format("HH:mm:ss [<small>]A[</small>]");
-}}, 1000);
-
+  if (africaDate) {
+    africaDate.innerHTML = moment
+      .tz("Africa/Johannesburg")
+      .format("MMMM Do, YYYY");
+    africaTime.innerHTML = moment
+      .tz("Africa/Johannesburg")
+      .format("HH:mm:ss [<small>]A[</small>]");
+  }
+}, 1000);
